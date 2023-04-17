c2d3fc98a7ee280fb7b14ca9704789f42e54a57c
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2023-04-17 17:04:37 +0300

    refactor train run scripts

--- siamese_tracking/onekey_fc.py
+++ siamese_tracking/onekey_fc.py
@@ -19,7 +19,7 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='Train SiamFC with onekey')
     # for train
-    parser.add_argument('--cfg', type=str, default='experiments/train/SiamFC.yaml', help='yaml configure file name')
+    parser.add_argument('--cfg', type=str, default='experiments/train/DroneCfg.yaml', help='yaml configure file name')
 
     # for
 
@@ -42,19 +42,19 @@ def main():
     # epoch training -- train 50 or more epochs
     if trainINFO['ISTRUE']:
         print('==> train phase')
-        print('python ./siamese_tracking/train_siamfc.py --cfg {0} --gpus {1} --workers {2} 2>&1 | tee logs/siamfc_train.log'
+        print('python3 ./siamese_tracking/train_siamfc.py --cfg {0} --gpus {1} --workers {2} 2>&1 | tee logs/siamfc_train.log'
                   .format(args.cfg, info['GPUS'], info['WORKERS']))
 
         if not exists('logs'):
             os.makedirs('logs')
 
-        os.system('python ./siamese_tracking/train_siamfc.py --cfg {0} --gpus {1} --workers {2} 2>&1 | tee logs/siamrpn_train.log'
+        os.system('python3 ./siamese_tracking/train_siamfc.py --cfg {0} --gpus {1} --workers {2} 2>&1 | tee logs/siamrpn_train.log'
                   .format(args.cfg, info['GPUS'], info['WORKERS']))
 
-    # epoch testing -- test 30-50 epochs (or more)
+    '''
     if testINFO['ISTRUE']:
         print('==> test phase')
-        print('mpiexec -n {0} python ./siamese_tracking/test_epochs.py --arch {1} --start_epoch {2} --end_epoch {3} --gpu_nums={4} \
+        print('mpiexec -n {0} python3 ./siamese_tracking/test_epochs.py --arch {1} --start_epoch {2} --end_epoch {3} --gpu_nums={4} \
                   --threads {0} --dataset {5}  2>&1 | tee logs/siamfc_epoch_test.log'
                   .format(testINFO['THREADS'], trainINFO['MODEL'], testINFO['START_EPOCH'], testINFO['END_EPOCH'],
                           (len(info['GPUS']) + 1) // 2, testINFO['DATA']))
@@ -62,40 +62,19 @@ def main():
         if not exists('logs'):
             os.makedirs('logs')
 
-        os.system('mpiexec -n {0} python ./siamese_tracking/test_epochs.py --arch {1} --start_epoch {2} --end_epoch {3} --gpu_nums={4} \
+        os.system('mpiexec -n {0} python3 ./siamese_tracking/test_epochs.py --arch {1} --start_epoch {2} --end_epoch {3} --gpu_nums={4} \
                   --threads {0} --dataset {5}   2>&1 | tee logs/siamfc_epoch_test.log'
                   .format(testINFO['THREADS'], trainINFO['MODEL'], testINFO['START_EPOCH'], testINFO['END_EPOCH'],
                           (len(info['GPUS']) + 1) // 2, testINFO['DATA']))
 
         # test on vot or otb benchmark
         if 'OTB' in testINFO['DATA']:
-            os.system('python ./lib/core/eval_otb.py {0} ./result SiamFC* 0 100 2>&1 | tee logs/siamfc_eval_epochs.log'.format(testINFO['DATA']))
+            os.system('python3 ./lib/core/eval_otb.py {0} ./result SiamFC* 0 100 2>&1 | tee logs/siamfc_eval_epochs.log'.format(testINFO['DATA']))
         elif 'VOT' in testINFO['DATA']:
-            os.system('python ./lib/core/eval_vot.py {0} ./result 2>&1 | tee logs/siamfc_eval_epochs.log'.format(testINFO['DATA']))
+            os.system('python3 ./lib/core/eval_vot.py {0} ./result 2>&1 | tee logs/siamfc_eval_epochs.log'.format(testINFO['DATA']))
         else:
             print("CUSTOM")
-
-
-    # tuning -- with TPE
-    if tuneINFO['ISTRUE']:
-
-        if 'OTB' in testINFO['DATA']:
-            pass
-        elif 'VOT' in testINFO['DATA'] and 'LT' not in testINFO['DATA']:   # for vot real-time and baseline
-            resume = extract_logs('logs/siamfc_eval_epochs.log', 'VOT')
-        elif 'LT' in testINFO['DATA']:                                     # for vot longterm
-            pass
-        else:
-            raise ValueError('not supported now')
-
-        print('==> tune phase')
-        print('python -u ./siamese_tracking/tune_tpe.py --arch {0} --resume {1} --dataset {2} --gpu_nums {3} \
-                  2>&1 | tee logs/tpe_tune_fc.log'.format(trainINFO['MODEL'], 'snapshot/'+ resume, tuneINFO['DATA'], (len(info['GPUS']) + 1) // 2))
-
-        if not exists('logs'):
-            os.makedirs('logs')
-        os.system('python -u ./siamese_tracking/tune_tpe.py --arch {0} --resume {1} --dataset {2} --gpu_nums {3}\
-                  2>&1 | tee logs/tpe_tune_fc.log'.format(trainINFO['MODEL'], 'snapshot/'+ resume, tuneINFO['DATA'], (len(info['GPUS']) + 1) // 2))
+    '''
 
 
 if __name__ == '__main__':
